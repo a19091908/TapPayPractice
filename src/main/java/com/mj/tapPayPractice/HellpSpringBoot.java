656450f93bc364903fcd5d90a1e19076a04dee08
maggieChang <maggiechang.w@gmail.com>
2019-09-26 03:56:01 +0800

    add Controller & templates

---
+++ src/main/java/com/mj/tapPayPractice/HellpSpringBoot.java
@@ -0,0 +1,13 @@
+package com.mj.tapPayPractice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellpSpringBoot {
+	//透過 @RequestMapping 指定從/會被對應到此hello()方法
+	  @RequestMapping("/")
+	  public String hello(){
+	      return "向全世界說聲Spring Boot 很高興認識你!";
+	  }
+}
