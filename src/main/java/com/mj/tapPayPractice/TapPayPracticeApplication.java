a9aedf8d18f5a54dd799df1c2a8eb6ec5e9ae923
a19091908 <a19091908@gmail.com>
2019-09-26 00:48:28 +0800

    init

---
+++ src/main/java/com/mj/tapPayPractice/TapPayPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mj.tapPayPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapPayPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapPayPracticeApplication.class, args);
+	}
+
+}
