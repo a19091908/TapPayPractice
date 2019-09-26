e3797910bd6bc181a227ae136cf5b05fd7f013f2
maggieChang <maggiechang.w@gmail.com>
2019-09-26 11:57:57 +0800

    fix

--- src/main/java/com/mj/tapPayPractice/Service/UserService.java
+++ src/main/java/com/mj/tapPayPractice/Service/UserService.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package com.mj.tapPayPractice.Service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-	public Integer getUserId() {
-		System.out.println("進入UserService的getUserId方法");
-		int userId = 402170532;
-		return userId;
-	}
-}
-=======
 package com.mj.tapPayPractice.Service;
 
 import org.springframework.stereotype.Service;
@@ -25,4 +11,3 @@ public class UserService {
 		return userId;
 	}
 }
->>>>>>> branch 'master' of https://github.com/a19091908/TapPayPractice
