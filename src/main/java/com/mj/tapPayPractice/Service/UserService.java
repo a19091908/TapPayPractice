5e4795fb0cfa6db51c813dbe78e2ceb85d8fc092
maggieChang <maggiechang.w@gmail.com>
2019-09-26 11:49:28 +0800

    fix

--- src/main/java/com/mj/tapPayPractice/Service/UserService.java
+++ src/main/java/com/mj/tapPayPractice/Service/UserService.java
@@ -4,17 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-<<<<<<< HEAD
-	public Integer getUserId(){
-		  System.out.println("進入了UserService的getUserId方法");
-		  int userId=5;
-		  return userId;
-	  }
-=======
+
 	public Integer getUserId() {
 		System.out.println("進入UserService的getUserId方法");
 		int userId = 402170532;
 		return userId;
 	}
->>>>>>> 7ccbcde45b62ba59b169185cafe65aa77baf2a44
 }
